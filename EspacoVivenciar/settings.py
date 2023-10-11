832ecd143eb6667f76ef0bb5c1f2599a2fcbb510
Valéria <vvqf2016@gmail.com>
2023-10-10 23:29:43 -0300

    started developing the landing page

--- EspacoVivenciar/settings.py
+++ EspacoVivenciar/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'apps/core/static',
+) 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
