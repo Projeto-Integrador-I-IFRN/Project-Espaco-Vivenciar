6a187fd1e7897ef8af1755d628eccfb1c06482d4
Henrique <heduardocr7@gmail.com>
2023-10-13 21:45:45 -0300

    modification to core app files

--- EspacoVivenciar/settings.py
+++ EspacoVivenciar/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core',
+    'apps.perfil',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
+    BASE_DIR / 'static',
     BASE_DIR / 'apps/core/static',
+    BASE_DIR / 'apps/perfil/static',
 ) 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
