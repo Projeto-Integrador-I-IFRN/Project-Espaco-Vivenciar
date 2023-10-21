5f31f035837753f9d69932349c3edb23785d23b9
Henrique <heduardocr7@gmail.com>
2023-10-21 20:57:07 -0300

    deu certo

--- apps/atendente/urls.py
+++ apps/atendente/urls.py
@@ -6,4 +6,5 @@ app_name = 'atendente'
 urlpatterns = [
     path("atendente/home/", views.Home, name="Home"),
     path("atendente/gerenciar/", views.Gerenciar, name="Gerenciar"),
+    path("atendente/solicitacoes/", views.Solicitacoes, name="Gerenciar"),
 ]
--- apps/atendente/views.py
+++ apps/atendente/views.py
@@ -21,3 +21,24 @@ def Gerenciar(request):
         'edit': True
     }
     return render(request, 'atendente/cadastrar_agenda.html', context=context)
+
+def Solicitacoes(request):
+    context = {
+        'card': 'agendamento',
+        'data': True,
+        'info_user': True,
+        'info_user2': False,
+        'horario': True,
+        'horario2': True,
+        'lista_servico': True,
+        'button_solicitar': True,
+        'button_whatsApp': True,
+        'button_indeferido': False,
+        'button_recusar': False,
+        'button_aceitar': False,
+        'button_detalhar': False,
+        'count_agendados': False,
+        'count_solicitacoes': False,
+        'edit': True
+    }
+    return render(request, 'atendente/solicitacoes_consultas.html', context=context)
