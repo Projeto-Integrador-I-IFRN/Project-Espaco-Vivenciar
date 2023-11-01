f8e7fca48644fa3432b7c07265c227b97d871d18
Valéria <vvqf2016@gmail.com>
2023-11-01 01:51:08 -0300

    modified input focus color

--- apps/atendente/static/atendente/js/cadastro-paciente.js
+++ apps/atendente/static/atendente/js/cadastro-paciente.js
@@ -1,39 +1,70 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const editButton = document.querySelector("#editButton")
-    const editFields = document.querySelectorAll(".edit-field")
-    const viewFields = document.querySelectorAll(".view-field")
-    const imagePacient = document.querySelector(".image-paciente")
-    const firstSection = document.querySelector(".first-section")
-    const newPacient = document.querySelector('.cadastrar-paciente-button')
-    const cardPacientes = document.querySelector(".pacientes-detalhados")
-    const buttonExcluir = document.querySelector('#button-excluir')
-    const pacientList = document.querySelectorAll('.card-paciente')
-    
-    newPacient.addEventListener("click", function()
-    {
-        
+    const editButton = document.querySelector("#editButton");
+    const editFields = document.querySelectorAll(".edit-field");
+    const viewFields = document.querySelectorAll(".view-field");
+    const imagePacient = document.querySelector(".image-paciente");
+    const firstSection = document.querySelector(".first-section");
+    const newPacient = document.querySelector('.cadastrar-paciente-button');
+    const cardPacientes = document.querySelector(".pacientes-detalhados");
+    const buttonExcluir = document.querySelector('#button-excluir');
+    const pacientList = document.querySelectorAll('.card-paciente');
+
+    newPacient.addEventListener("click", function () {
+        editButton.textContent = "Salvar";
+            editButton.style.backgroundColor = "var(--green-base)";
+            editButton.style.color = "var(--background-white)";
+            editButton.style.border = "none";
+            editButton.style.fontWeight = "500";
+            imagePacient.style.display = "none";
+            firstSection.style.gap = "100px";
+        cardPacientes.style = "";
+        editFields.forEach((field) => {
+            field.style.display = "block";
+            if (field.tagName === "SELECT") {
+                // Mantém o valor do campo de gênero
+                field.value = "Masculino";
+            } else {
+                field.value = '';
+            }
+        });
+
+        viewFields.forEach((field) => {
+            field.style.display = "none";
+        });
+
+        imagePacient.style.display = "none";
+        firstSection.style.gap = "100px";
     });
 
     buttonExcluir.addEventListener("click", function () {
-        cardPacientes.style.visibility = "hidden"
+        cardPacientes.style.visibility = "hidden";
     });
 
-    pacientList.forEach(function(pacient)
-    {
-        pacient.addEventListener("click", function () 
-        {
-            cardPacientes.style = ""
+    pacientList.forEach(function (pacient) {
+        pacient.addEventListener("click", function () {
+            cardPacientes.style = "";
+            editFields.forEach((field) => {
+                field.style.display = "none";
+            });
+    
+            viewFields.forEach((field) => {
+                field.style.display = "block";
+            });
+    
+            editButton.textContent = "Editar";
+            editButton.removeAttribute("style");
+            imagePacient.style.display = "block";
+            firstSection.style.removeProperty("gap");
         });
     });
-    
+
     editButton.addEventListener("click", function () {
+        
         if (editButton.textContent === "Editar") {
-            // Alternar para campos de edição
             editFields.forEach((field) => {
                 field.style.display = "block";
             });
 
-            // Ocultar campos de visualização
             viewFields.forEach((field) => {
                 field.style.display = "none";
             });
@@ -46,20 +77,18 @@ document.addEventListener("DOMContentLoaded", function () {
             imagePacient.style.display = "none";
             firstSection.style.gap = "100px";
         } else if (editButton.textContent === "Salvar") {
-            // Alternar para campos de visualização
             viewFields.forEach((field) => {
                 field.style.display = "block";
             });
 
-            // Ocultar campos de edição
             editFields.forEach((field) => {
                 field.style.display = "none";
             });
 
             editButton.textContent = "Editar";
-            editButton.removeAttribute("style"); // Remove estilos personalizados
+            editButton.removeAttribute("style");
             imagePacient.style.display = "block";
-            firstSection.style.removeProperty("gap"); // Remove a propriedade gap
+            firstSection.style.removeProperty("gap");
         }
     });
 });
