acacffdcf044c23ba7003f1990355ab8cf8b7a0e
Valéria <vvqf2016@gmail.com>
2023-11-23 10:59:15 -0300

    creating Models

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+    pass
+
+class Atendente(models.Model):
+    pass
+
+class Paciente(models.Model):
+    nome_paciente = models.CharField('Nome', max_length= 200)
+    data_nascimento = models.DateField('Data de Nascimento')
+    email = models.EmailField(max_length= 200)
+    cpf_paciente = models.CharField(max_length= 11)
+
+    def __str__(self):
+        return f'Nome: {self.nome_paciente} CPF: {self.cpf_paciente}'
+
+class Profissional(models.Model):
+    pass
+
+class Servico(models.Model):
+    pass
