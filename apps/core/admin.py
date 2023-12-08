24ec9527c467cdaeda89864c1e4801a845d9dc85
Valéria <vvqf2016@gmail.com>
2023-12-08 14:52:42 -0300

    removing files :fire:

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
-from .models import Paciente, Profissional, Servicos, Agendamento
+from .models import Paciente, Profissional, Servico, Agendamento
 
-@admin.register(Paciente)
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = ('nome_paciente', 'data_nascimento', 'genero', 'email', 'cpf_paciente')
-
-@admin.register(Profissional)
-class ProfissionalAdmin(admin.ModelAdmin):
-    list_display = ('nome_medico', 'especialidade', 'horario_inicial', 'horario_final')
-
-@admin.register(Servicos)
-class ServicosAdmin(admin.ModelAdmin):
-    list_display = ('nome_servico', 'duracao_servico')
-
-@admin.register(Agendamento)
-class AgendamentoAdmin(admin.ModelAdmin):
-    list_filter = ['profissional']
+admin.site.register(Paciente)
+admin.site.register(Profissional)
+admin.site.register(Servico)
+admin.site.register(Agendamento)
