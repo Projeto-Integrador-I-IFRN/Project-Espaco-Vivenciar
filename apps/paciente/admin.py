da2a89758a96c7dfe958021e037a9961d1169902
Henrique <heduardocr7@gmail.com>
2023-12-11 18:07:42 -0300

    correction in admins

--- apps/paciente/admin.py
+++ apps/paciente/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Paciente
 
 # Register your models here.
-@admin.register(Paciente)
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = ('nome_paciente', 'data_nascimento', 'genero', 'email', 'cpf_paciente')
