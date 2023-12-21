5a52bf0606fd873ac739caad2ec18c2b636b919b
Valéria <vvqf2016@gmail.com>
2023-12-21 16:14:54 -0300

    fixed open modal

--- apps/medico/static/medico/js/modal.js
+++ apps/medico/static/medico/js/modal.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const abrirModal = document.getElementById("abrirModal");
-    const abrirModal2 = document.getElementById("abrirModal2");
+    const abrirModal2Elements = document.getElementsByClassName("abrirModal2");
     const abrirModal3Elements = document.getElementsByClassName("abrirModal3");
     const modal = document.getElementById("myModal");
-    const modal2 = document.getElementById("myModal2");
-    const modal3 = document.getElementsByClassName("myModal3")[0]; // Selecting the first element
+    const modal2 = document.getElementsByClassName("myModal2")[0];
+    const modal3 = document.getElementsByClassName("myModal3")[0];
+    
 
     if (abrirModal) {
         abrirModal.addEventListener("click", () => abrirModalHandler(modal));
-        abrirModal2.addEventListener("click", () => abrirModalHandler(modal2));
     }
 
-    else if (abrirModal3Elements) {
+    if (abrirModal2Elements.length > 0) {
+        for (const abrirModal2 of abrirModal2Elements) {
+            abrirModal2.addEventListener("click", () => abrirModalHandler(modal2));
+        }
+    }
 
+    if (abrirModal3Elements.length > 0) {
         for (const abrirModal3 of abrirModal3Elements) {
             abrirModal3.addEventListener("click", () => abrirModalHandler(modal3));
         }
-        
     }
 
     function abrirModalHandler(modal) {
         if (typeof modal.showModal === 'function') {
             console.log(`Abrir Modal ${modal.id}!!`);
-
             modal.showModal();
 
             modal.addEventListener("click", (event) => {
