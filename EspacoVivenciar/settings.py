5b1c976cdac67020a4a41e92b954c4bf42fd47ad
Valéria <vvqf2016@gmail.com>
2023-12-25 22:57:24 -0300

    troquei banco

--- EspacoVivenciar/settings.py
+++ EspacoVivenciar/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'EspacoVivenciar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EspacoVivenciar',
-        'USER': 'postgres',
-        'PASSWORD': '1214212530',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
